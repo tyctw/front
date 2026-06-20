5adc5bf357f33c0d08eb96d34877df91b42189ab
tyctw <tyctw.analyze@gmail.com>
2026-06-21 01:13:31 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const ResultReminderModal = lazy(() => import("./components/Modals").then(m => (
 const ShareModal = lazy(() => import("./components/Modals").then(m => ({ default: m.ShareModal })));
 
 const RESULT_LIST_OPEN_DATE = "2026-07-05T11:00:00";
-const RESULT_REMINDER_START_DATE = "2026-06-21T00:00:00";
+const RESULT_REMINDER_START_DATE = "2026-07-01T00:00:00";
 const RESULT_REMINDER_END_DATE = "2026-07-15T23:59:59";
 
 export default function App() {
